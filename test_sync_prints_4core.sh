b1dac7ff0ed6c6b8ccea908578ba07f15cc86d9e
NILOIDE <nilstoltanso@gmail.com>
2019-03-21 03:22:54 +0100

    Increased mem

--- test_sync_prints_4core.sh
+++ test_sync_prints_4core.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --time=0-00:03:00
-#SBATCH --mem=10
+#SBATCH --time=0-00:10:00
+#SBATCH --mem=1000
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=4
 #SBATCH --output=test_sync_prints_4core.out

a9c4fc6ef2ea4b36cf4832451de53a61e80d1345
NILOIDE <nilstoltanso@gmail.com>
2019-03-21 03:55:01 +0100

    Mistake in file .out nameing

--- test_sync_prints_1core.sh
+++ test_sync_prints_1core.sh
@@ -3,5 +3,5 @@
 #SBATCH --mem=1000
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
-#SBATCH --output=test_sync_1core.out
+#SBATCH --output=test_sync_prints_1core.out
 python ./peregrine_test_synchronized_with_prints.py
